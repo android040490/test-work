7c32c270e6afb630f1471ca7880712e963892f06
Pypkin <apalkov040490@gmail.com>
2018-04-19 23:26:56 +0300

    changed authorization form validation

--- src/app.js
+++ src/app.js
@@ -3,45 +3,52 @@ import * as jQuery from "jquery";
 
 
 $(function(){
+    
     $('#forgot-pass').mouseenter(function(){
-        $('.facebook-login__facebook-icon').animate({ height : "130%"}, 300);
-    })
+            $('.facebook-login__facebook-icon svg').addClass("zoom-icon");
+    });
     $('#forgot-pass').mouseleave(function(){
-        $('.facebook-login__facebook-icon').animate({ height : "100%"}, 300);
-    })
+        $('.facebook-login__facebook-icon svg').removeClass("zoom-icon");
+});
+
 
     // Forms validation
     var emailValid = false;
     var passValid = false;
 
-    $("input[type='email']").keypress(function() {
+    
+    function validationForm(event){
+        var elementForm = $(event.target);
+        var elementValue = elementForm.val();
         
-        if($(this).val() != '') {
+        if( elementForm.attr('type') == "email"){
             var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-            if(pattern.test( $(this).val())){
+            if(pattern.test( elementForm.val())){
                 emailValid = true;
-                $(this).css({'border' : '2px solid rgb(47, 197, 47)'});
+                elementForm.css({'border' : '2px solid rgb(47, 197, 47)'});
             } else{
-                $(this).css({'border' : '2px solid rgb(200, 0, 0)'});
+                emailValid = false;
+                elementForm.css({'border' : '2px solid rgb(200, 0, 0)'});
             };
-        };
-        if (emailValid && passValid){
-            $(".authorization-form__button input").css( {'background': 'rgb(32, 70, 197)'})
-        }
-    });
-    $("input[type='password']").keypress(function() {
-        if($(this).val() != '') {
-            if($(this).val.length > 0){
+        } else{
+            
+            if( elementValue.length > 1 ){
                 passValid = true;
-                $(this).css({'border' : '2px solid rgb(47, 197, 47)'});
+                elementForm.css({'border' : '2px solid rgb(47, 197, 47)'});
             } else{
-                $(this).css({'border' : '2px solid rgb(200, 0, 0)'});
-            };
-        
-        };
+                passValid = false;
+                elementForm.css({'border' : '2px solid rgb(200, 0, 0)'});
+            }
+        }
         if (emailValid && passValid){
-            $(".authorization-form__button input").css( {'background': 'rgb(32, 70, 197)'})
+            $(".authorization-form__button input").addClass( "active-btn").removeAttr('disabled');
+        
+        } else {
+            $(".authorization-form__button input").removeClass( "active-btn").attr('disabled', 'disabled');
         };
-    });
+    };
+
+    $("input[type='password'], input[type='email']").on("input", validationForm);
+    
     
 });
